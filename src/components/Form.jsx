41e3a57887cc9c6acb280b31d91ca9b0fa3bf15c
Muhammad Ayaad <ayaadalavi@gmail.com>
2024-03-19 00:22:26 +0500

    first commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Button } from "react-bootstrap"; // Import Bootstrap components
 import "./form.css";
+import { Modal, Button, Alert } from "react-bootstrap";
+import { FaComments, FaRobot, FaBrain } from "react-icons/fa";
+import { IoPeople } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 function Form() {
   const [formData, setFormData] = useState({
@@ -9,14 +13,16 @@ function Form() {
     passwordInput: "",
     textareaInput: "",
     selectInput: "",
-    radioInput: "",
-    checkboxInput1: false,
-    checkboxInput2: false,
+    genderInput: "",
+    humanInput: false,
+    aiInput: false,
   });
 
-  const [errors, setErrors] = useState({});
-  const [submissionStatus, setSubmissionStatus] = useState(null);
-  const [showModal, setShowModal] = useState(false); // State for controlling modal visibility
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
     const handleKeyPress = (event) => {
@@ -32,207 +38,172 @@ function Form() {
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
-    const newValue = type === "checkbox" ? checked : value;
-    setFormData({ ...formData, [name]: newValue });
+    let newValue = type === "checkbox" ? checked : value;
 
-    if (name === "textInput" && value.trim() === "") {
-      setErrors({ ...errors, [name]: "Text Input is required" });
-    } else if (name === "emailInput" && !isValidEmail(value)) {
-      setErrors({ ...errors, [name]: "Invalid Email" });
-    } else {
-      setErrors({ ...errors, [name]: "" });
+    if (name === "textInput") {
+      const hasSpecialChars = /[!@#$%^&*()_+\\|}\]{[:"><?';~`]/.test(value); // Regex for special characters
+      setNameError(hasSpecialChars ? "Special characters are not allowed" : "");
     }
+
+    if (name === "emailInput") {
+      const isValidFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value); // Regex for email format
+      setEmailError(!isValidFormat ? "Invalid Email" : "");
+    }
+
+    if (name === "passwordInput") {
+      const isValidPassword =
+        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(value); // Regex for password format
+      setPasswordError(
+        !isValidPassword
+          ? "Password must be 6-16 characters long and contain at least one digit and one special character"
+          : ""
+      );
+    }
+
+    setFormData({ ...formData, [name]: newValue });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    for (const key in formData) {
-      if (formData.hasOwnProperty(key)) {
-        if (key === "textInput" && formData[key].trim() === "") {
-          setErrors({ ...errors, [key]: "Text Input is required" });
-          return;
-        } else if (key === "emailInput" && !isValidEmail(formData[key])) {
-          setErrors({ ...errors, [key]: "Invalid Email" });
-          return;
-        }
-      }
+    if (Object.values(formData).some((value) => value === "")) {
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 1000);
+      return;
     }
-    simulateFormSubmission(formData);
-  };
-
-  const simulateFormSubmission = (formData) => {
-    setTimeout(() => {
-      const isSuccess = Math.random() < 0.8;
-      if (isSuccess) {
-        setSubmissionStatus("success");
-        setShowModal(true);
-      } else {
-        setSubmissionStatus("error");
-      }
-    }, 1000);
-  };
-
-  const isValidEmail = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    setShowModal(true);
   };
 
   const closeModal = () => {
     setShowModal(false);
-    setSubmissionStatus(null);
+  };
+
+  const fieldLabels = {
+    textInput: "Name",
+    emailInput: "Email",
+    passwordInput: "Password",
+    textareaInput: "Comments",
+    selectInput: "Country",
+    genderInput: "Gender",
+    humanInput: "Human",
+    aiInput: "AI",
   };
 
   return (
     <div>
-      {submissionStatus === "error" && (
-        <div className="alert alert-danger">
-          Form submission failed. Please try again.
-        </div>
+      {showAlert && (
+        <Alert variant="danger">Fill all the Fields Before submitting</Alert>
+      )}
+      {(nameError || emailError || passwordError) && (
+        <Alert variant="danger">
+          {nameError && <div>{nameError}</div>}
+          {emailError && <div>{emailError}</div>}
+          {passwordError && <div>{passwordError}</div>}
+        </Alert>
       )}
 
-      {/* Error messages container */}
-      <div>
-        {Object.keys(errors).map((key) => (
-          <div className="alert alert-danger" key={key}>
-            {errors[key]}
-          </div>
-        ))}
-      </div>
-
       <form className="form-container" onSubmit={handleSubmit}>
-        <label>
-          <p> Name:</p> <br />
-          <input
-            type="text"
-            name="textInput"
-            value={formData.textInput}
-            onChange={handleChange}
-          />
-          {errors.textInput && (
-            <div className="alert alert-danger">{errors.textInput}</div>
-          )}
-        </label>
-        <label>
-          <p>Email:</p> <br />
-          <input
-            type="email"
-            name="emailInput"
-            value={formData.emailInput}
-            onChange={handleChange}
-          />
-          {errors.emailInput && (
-            <div className="alert alert-danger">{errors.emailInput}</div>
-          )}
-        </label>
-        <label>
-          <p>Password:</p> <br />
-          <input
-            type="password"
-            name="passwordInput"
-            value={formData.passwordInput}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p>Textarea:</p> <br />
-          <textarea
-            name="textareaInput"
-            value={formData.textareaInput}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p>Select Dropdown:</p> <br />
-          <select
-            name="selectInput"
-            value={formData.selectInput}
-            onChange={handleChange}
-          >
-            <option value="">Select One</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-          </select>
-        </label>
-        <div className="fifthLabel">
-          <label>
-            Option 1
-            <input
-              type="radio"
-              name="radioInput"
-              value="option1"
-              checked={formData.radioInput === "option1"}
-              onChange={handleChange}
-            />
+        {Object.keys(formData).map((key) => (
+          <label key={key}>
+            <p>
+              {fieldLabels[key]} {key === "textInput" && <IoPeople />}
+              {key === "emailInput" && <MdEmail />}
+              {key === "passwordInput" && <RiLockPasswordFill />}
+              {key === "textareaInput" && <FaComments />}
+            </p>{" "}
+            <br />
+            {key === "selectInput" ? (
+              <select name={key} value={formData[key]} onChange={handleChange}>
+                <option value="">Other</option>
+                <option value="option1">Albania</option>
+                <option value="option2">Kynea</option>
+                <option value="option3">Canada</option>
+              </select>
+            ) : key === "genderInput" ? (
+              <div className="genderBox">
+                <label>
+                  Male
+                  <input
+                    type="radio"
+                    name={key}
+                    value="Male"
+                    checked={formData[key] === "Male"}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label>
+                  Female
+                  <input
+                    type="radio"
+                    name={key}
+                    value="Female"
+                    checked={formData[key] === "Female"}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+            ) : key === "humanInput" || key === "aiInput" ? (
+              <div>
+                <label>
+                  {key === "humanInput" ? "Human" : "AI"}
+                  <input
+                    type="checkbox"
+                    name={key}
+                    checked={formData[key]}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+            ) : (
+              <input
+                type={key === "passwordInput" ? "password" : "text"}
+                name={key}
+                value={formData[key]}
+                onChange={handleChange}
+              />
+            )}
           </label>
-          <label>
-            Option 2
-            <input
-              type="radio"
-              name="radioInput"
-              value="option2"
-              checked={formData.radioInput === "option2"}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div className="fifthLabel">
-          <label>
-            Checkbox 1
-            <input
-              type="checkbox"
-              name="checkboxInput1"
-              checked={formData.checkboxInput1}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Checkbox 2
-            <input
-              type="checkbox"
-              name="checkboxInput2"
-              checked={formData.checkboxInput2}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
+        ))}
         <button type="submit">Submit</button>
       </form>
 
-      {submissionStatus === "success" && (
-        <Modal show={showModal} onHide={closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Form submitted successfully!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h2>Form Data</h2>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Field</th>
-                  <th>Value</th>
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {" "}
+            <p style={{ color: "green" }}>Form submitted successfully!</p>{" "}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2>Form Data</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Field</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(formData).map((key) => (
+                <tr key={key}>
+                  <td>{fieldLabels[key]}</td>
+                  <td>
+                    {typeof formData[key] === "boolean"
+                      ? formData[key].toString()
+                      : formData[key]}
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {Object.keys(formData).map((key) => (
-                  <tr key={key}>
-                    <td>{key}</td>
-                    <td>
-                      {typeof formData[key] === "boolean"
-                        ? formData[key].toString()
-                        : formData[key]}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={closeModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      )}
+              ))}
+            </tbody>
+          </table>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
